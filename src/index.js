588653a33f16b5557e333db0a5dd4432f12f04b8
tarz2929 <saitarunmulley@gmail.com>
2020-09-17 11:14:35 -0600

    added HelloStudnet comp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloStudent from './components/HelloStudent';
 import './index.css';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <h1>My Git Test</h1>
+    <HelloStudent name = "Eboka"/>
+    <HelloStudent name = "Preet"/>     
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
