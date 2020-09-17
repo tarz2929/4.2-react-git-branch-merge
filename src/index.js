f12934045ad359482bcd2bcf33fc7a132c855633
tarz2929 <saitarunmulley@gmail.com>
2020-09-17 10:52:19 -0600

    Added HelloWorld component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
 import './index.css';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>React git merge practice</h1>
+    <HelloWorld/>
     
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 
